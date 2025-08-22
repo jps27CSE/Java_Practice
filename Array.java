5337de862010c5ffe7da9282ffa5cce9dcf12486
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2025-08-22 19:11:41 +0600

    array

---
+++ Array.java
@@ -0,0 +1,19 @@
+public class Array {
+    public static void main(String[] args) {
+        int array [] = {1,2,3};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        int array1 [] = new int[4];
+
+        array1[0]  = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array1[i] + " ");
+        }
+    }
+}
