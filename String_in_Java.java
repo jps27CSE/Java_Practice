bd82225320ff9728b4902f3b713f7337ed696e70
jps27cse <jackpritomsoren@hotmail.com>
2025-09-21 11:54:06 +0600

    string in java

---
+++ String_in_Java.java
@@ -0,0 +1,11 @@
+public class String_in_Java {
+    public static void main(String[] args){
+        String string_name = new String("Jack");
+
+        System.out.println(string_name);
+
+
+        String name = "Pritom";
+        System.out.println(name);
+    }
+}
